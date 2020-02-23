347a2b0bba416005bc33d84360f1b1ca1ab0f59e
JLarky <jlarky@gmail.com>
2020-02-23 14:19:16 -0800

    store game state in localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
 import React from "react";
+import ErrorBoundary from "react-error-boundary";
 
 import "./App.css";
 
 import FindPicture from "./findPicture/FindPicture";
 
-const App: React.FC = () => {
-  return <FindPicture />
-};
+class App extends React.Component<
+  {},
+  {
+    errorBoundaryKey: number;
+    hasError: boolean;
+  }
+> {
+  state = {
+    errorBoundaryKey: 0,
+    hasError: false
+  };
+
+  handleResetButtonClick = () =>
+    this.setState(prevState => ({
+      errorBoundaryKey: prevState.errorBoundaryKey + 1,
+      hasError: false
+    }));
+
+  render() {
+    return !this.state.hasError ? (
+      <ErrorBoundary
+        key={this.state.errorBoundaryKey}
+        onError={() => {
+          localStorage.clear();
+          this.setState({ hasError: true });
+        }}
+      >
+        <FindPicture />
+      </ErrorBoundary>
+    ) : (
+      <div style={{ display: "grid", height: "100%" }}>
+        <button onClick={this.handleResetButtonClick}>
+          reset error boundary
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/findPicture/FindPicture.tsx
+++ src/findPicture/FindPicture.tsx
@@ -101,14 +101,17 @@ function reducer(draft: typeof initalState, action: Action) {
   return draft;
 }
 const FindPicture: React.FC = () => {
+  const firstState = loadCardsState();
   React.useEffect(() => {
     document.title = "Find Pictrure Game";
     window.addEventListener("beforeinstallprompt", e => {
       // Stash the event so it can be triggered later.
     });
   });
-  const [state, dispatch] = useImmerReducer(reducer, initalState);
-  console.log(JSON.stringify(state.cards), dispatch);
+  const [state, dispatch] = useImmerReducer(reducer, firstState);
+  React.useEffect(() => {
+    localStorage.setItem("cards", JSON.stringify(state.cards));
+  }, [state.cards]);
   const onClick = (i: number) => () => {
     dispatch({ type: "tiggle", i });
   };
@@ -139,5 +142,16 @@ const FindPicture: React.FC = () => {
     </div>
   );
 };
+
+function loadCardsState() {
+  const cardsJS = localStorage.getItem("cards");
+  if (cardsJS) {
+    const cards = JSON.parse(cardsJS);
+    const state = { ...initalState };
+    state.cards = cards;
+    return state;
+  }
+  return initalState;
+}
 
 export default FindPicture;
