af563e025a727d6705c03a960d5a91816a3cbcce
JLarky <jlarky@gmail.com>
2021-02-13 14:38:31 -0800

    add clock loading animation as separate button

--- src/Pages.tsx
+++ src/Pages.tsx
@@ -7,7 +7,7 @@ import FindPicture from "./findPicture/FindPicture";
 
 export const Pages: React.FC = () => {
   const [game, setGame] = React.useState<
-    "picture" | "calculator" | "keyboard" | "clock" | "allogator" | "xylophone"
+    "picture" | "calculator" | "keyboard" | "clock" | "clockLoading" | "allogator" | "xylophone"
   >();
   React.useEffect(() => {
     if (game === "keyboard") {
@@ -16,6 +16,9 @@ export const Pages: React.FC = () => {
       window.location.href = "https://h2bcq.csb.app/";
     } else if (game === "allogator") {
       window.location.href = "http://tibordp.github.io/gator-calculus/";
+    } else if (game === "clockLoading") {
+      window.location.href =
+        "https://cdn.dribbble.com/users/7040/screenshots/8214019/media/9d162bf2d3303da6f3e777bbae322b33.gif";
     }
   });
 
@@ -25,7 +28,7 @@ export const Pages: React.FC = () => {
     return <Calculator />;
   } else if (game === "xylophone") {
     return <Xylophone />;
-  } else if (game === "keyboard" || game === "clock") {
+  } else if (game === "keyboard" || game === "clock" || game === "clockLoading") {
     return null;
   }
 
@@ -36,6 +39,7 @@ export const Pages: React.FC = () => {
         ["keyboard", "Typing practice", "#37B6F6"],
         ["calculator", "Calc", "#37B6F6"],
         ["clock", "Clock", "#37B6F6"],
+        ["clockLoading", "Loading", "#37B6F6"],
         ["allogator", "Alligator", "#37B6F6"],
         ["xylophone", "Xylophone", "#37B6F6"],
       ] as const).map(([section, label, color]) => (
