89f94ad9673c1d20b71659cd9054abc37cef9956
JLarky <jlarky@gmail.com>
2023-09-10 22:09:33 -0600

    add vite-plugin-checker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
+import checker from "vite-plugin-checker";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  vite: {
+    plugins: [checker({ typescript: true })],
+  },
 });
